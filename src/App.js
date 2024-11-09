49e49a9ff59c5a9f975b5a8f1334666051ca86f1
María Alejandra Boada Rodriguez <76635361+MariaAlejandraBoadaRodriguez@users.noreply.github.com>
2024-11-09 13:14:22 -0500

    Publicacion 2.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Dashboard from './pages/Dashboard';
 import Contact from './pages/Contact';
